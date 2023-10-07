48130cf137bd4d020e50ae1315a6a7a26f6f707e
connor-seneca <cwsquires@myseneca.ca>
2023-10-07 12:18:37 -0400

    added some loggers

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../../src/app');
+const logger = require('../../src/logger');
 
 describe('GET /v1/fragments', () => {
   // If the request is missing the Authorization header, it should be forbidden
@@ -12,10 +13,18 @@ describe('GET /v1/fragments', () => {
   // Using a valid username/password pair should give a success result with a .fragments array
   test('authenticated users get a fragments array', async () => {
     const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
+    logger.info(`fragments: ${res.body.fragments}`);
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('ok');
     expect(Array.isArray(res.body.fragments)).toBe(true);
   });
 
-  // TODO: we'll need to add tests to check the contents of the fragments array later
+  test('getting with expand property set to true', async () => {
+    const res = await request(app)
+      .get('/v1/fragments/?expand=1')
+      .auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe('ok');
+    expect(Array.isArray(res.body.fragments)).toBe(true);
+  });
 });
