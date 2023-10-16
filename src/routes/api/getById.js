d326d3db2a09523b350a3989e9ca865acdded146
connor-seneca <cwsquires@myseneca.ca>
2023-10-16 12:51:57 -0400

    removed an unneccessary comment

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -7,5 +7,4 @@ module.exports = async (req, res) => {
   const fragment = await Fragment.byId(req.user, req.params.id);
   logger.debug({ fragment });
   res.status(200).json(createSuccessResponse({ fragment }));
-  //}
 };
